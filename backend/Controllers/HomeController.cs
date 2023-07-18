f7c9c2efa7faa8449bd1b2434147cce80a85cd9d
mezeipb <mezei.peter.barnabas@gmail.com>
2023-07-18 11:03:51 +0200

    Init backend

---
+++ backend/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+
+namespace backend.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return Content("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return Content("Privacy");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
