c29ea07d15712daab80ff504fb9db40e7229ea83
mezeipb <mezei.peter.barnabas@gmail.com>
2023-07-20 15:13:53 +0200

    Extract Jwt-related methods in FriendHub.cs

--- backend/Hubs/FriendHub.cs
+++ backend/Hubs/FriendHub.cs
@@ -14,22 +14,38 @@ public class FriendHub : Hub
         _dbContext = dbContext;
     }
 
-    public async Task PingAll(string msg)
-        => await Clients.All.SendAsync("ReceivePing", msg);
+    [Obsolete]
+    private static IDictionary<string, object> DecodeJwt(string jwt)
+    {
+        try
+        {
+            return JwtBuilder.Create()
+                            .WithAlgorithm(new HMACSHA256Algorithm())
+                            .WithSecret("TEST_SECRET")
+                            .MustVerifySignature()
+                            .Decode<IDictionary<string, object>>(jwt);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    private static string GetUserNameFromIDictionary(IDictionary<string, object> dict)
+    {
+        string userName = dict["UserName"]?.ToString() ?? "";
+        if (userName == "")
+        {
+            throw new Exception("No valid username found in Dictionary.");
+        }
+        return userName;
+    }
 
     [Obsolete]
     public async Task AddToSelfGroup(string userToken)
     {
-        IDictionary<string, object> details = JwtBuilder.Create()
-                                            .WithAlgorithm(new HMACSHA256Algorithm())
-                                            .WithSecret("TEST_SECRET")
-                                            .MustVerifySignature()
-                                            .Decode<IDictionary<string, object>>(userToken);
-        string userName = details["UserName"]?.ToString() ?? "";
-        if (userName == "")
-        {
-            return;
-        }
+        IDictionary<string, object> details = DecodeJwt(userToken);
+        string userName = GetUserNameFromIDictionary(details);
         await Groups.AddToGroupAsync(Context.ConnectionId, userName);
     }
 }
