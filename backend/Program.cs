2a53b4b730b089ac9bc989ca11713a19f7387d20
mezeipb <mezei.peter.barnabas@gmail.com>
2023-07-20 12:23:51 +0200

    Make working SignalR ping

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,6 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<GigaChatDbContext>(options => options.UseSqlite("Data Source=app.db"));
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("https://example.com")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .AllowCredentials();
+        });
+});
 
 var app = builder.Build();
 
@@ -30,6 +41,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseCors();
 app.MapHub<ChatHub>("/Chat");
 
 app.Run();
--- frontend/src/Components/UserMainPage.tsx
+++ frontend/src/Components/UserMainPage.tsx
@@ -1,4 +1,34 @@
+import { HubConnection, HubConnectionBuilder, HubConnectionState, LogLevel } from "@microsoft/signalr";
+import { useEffect, useState } from "react";
+
 function UserMainPage() {
+    const [connection, setConnection] = useState<HubConnection | null>(null);
+
+    useEffect(() => {
+        const hubConnection = new HubConnectionBuilder()
+            .withUrl("/api/Chat")
+            .configureLogging(LogLevel.Information)
+            .withAutomaticReconnect()
+            .build();
+        setConnection(hubConnection);
+
+        () => setConnection(null);
+    }, []);
+
+    useEffect(() => {
+        if (!connection) {
+            return;
+        }
+        connection?.on("ReceivePing", (msg) => {
+            console.log("Ping message received from SignalR: " + msg);
+        });
+        if (connection.state === HubConnectionState.Disconnected) {
+            connection?.start()
+                .then(() => connection?.invoke("PingAll", "Hello, SignalR!"))
+        }
+
+    }, [connection]);
+
     return (
         <div>User Main Page</div>
     );
